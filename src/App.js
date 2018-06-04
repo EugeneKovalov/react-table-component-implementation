5df2ab3dc0dee72ccaa2c917c9173b84e72f70f9
EugeneKovalov <drawnowl@gmail.com>
2018-06-04 11:52:45 +0300

    Rendering data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./TableItem/TableItem";
+import TableItem from "./TableItem/TableItem";
 
 class App extends Component {
+  state = {
+    tableData: [
+      { id: "ad", title: "Germany" },
+      { id: "ae", title: "Italy" },
+      { id: "af", title: "Spain" },
+      { id: "ag", title: "Portugal" },
+      { id: "ah", title: "Austria" },
+      { id: "ai", title: "Denmark" },
+      { id: "au", title: "England" },
+      { id: "ak", title: "Ireland" }
+    ]
+  };
+
   render() {
+    let myData = (
+      <tr>
+        {this.state.tableData.map((data, index) => {
+          return <TableItem key={data.id} title={data.title} />;
+        })}
+      </tr>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+          <thead>
+            <tr>
+              <td>
+                <button>Create new data</button>
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+           { myData }
+          </tbody>
+        </table>
       </div>
     );
   }
