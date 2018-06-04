d72b7de9ec82307d8fd6e44c2fe361b68c4e70ab
EugeneKovalov <drawnowl@gmail.com>
2018-06-04 14:37:24 +0300

    Edit and Create functionality added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import TableItem from "./TableItem/TableItem";
 class App extends Component {
   state = {
     tableData: [
-      { id: "ad", title: "Germany" },
-      { id: "ae", title: "Italy" },
-      { id: "af", title: "Spain" },
-      { id: "ag", title: "Portugal" }
+      { id: 1, title: "Germany" },
+      { id: 2, title: "Italy" },
+      { id: 3, title: "Spain" },
+      { id: 4, title: "Portugal" }
     ]
   };
 
@@ -27,15 +27,92 @@ class App extends Component {
 
         this.setState({ tableData: items });
       },
+      
       error => {
-        console.log('delete error');  
+        console.log("delete error");
       }
     );
   };
 
+  changeTitleHandler = id => {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(id + " title was changed");
+      }, 1000);
+    });
+
+    promise.then(
+      result => {
+        const itemIndex = this.state.tableData.findIndex(i => {
+          return i.id === id;
+        });
+
+        const item = {
+          ...this.state.tableData[itemIndex]
+        };
+
+        let changeInputElements = document.querySelectorAll(".changeTitle");
+
+        item.title = changeInputElements[itemIndex].value;
+
+        for (let element of changeInputElements.values()) {
+          element.value = '';
+        }
+
+        const items = [...this.state.tableData];
+        items[itemIndex] = item;
+
+        this.setState({ tableData: items });
+      },
+
+      error => {
+        console.log("error");
+      }
+    );
+  };
+
+  createNewData = () => {
+      let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("New item was created");
+      }, 1000);
+    });
+
+    promise.then(
+      result => {
+        let newTitleElement = document.querySelector('#newElementTitle');
+
+        if (newTitleElement.value) {
+          const items = [...this.state.tableData];
+          items.push(
+            {
+              id: '_' + Math.random().toString(36).substr(2, 9), 
+              title: newTitleElement.value 
+            }
+          );
+
+          newTitleElement.value = '';
+          this.setState({ tableData: items });
+          console.log(this.state.tableData);
+        }
+      },
+      
+      error => {
+        console.log("create error");
+      }
+    );
+  }
+
   render() {
     let myData = this.state.tableData.map((data, index) => {
-          return <TableItem key={data.id} title={data.title} delete={() => this.deleteItemHandler(index) } />;
+      return (
+        <TableItem
+          key={data.id}
+          title={data.title}
+          delete={() => this.deleteItemHandler(index)}
+          changeTitle={() => this.changeTitleHandler(data.id)}
+        />
+      );
     });
 
     return (
@@ -44,13 +121,12 @@ class App extends Component {
           <thead>
             <tr>
               <td>
-                <button>Create new data</button>
+                <input type="text" id="newElementTitle" placeholder="Title of new element" />
+                <button onClick={() => this.createNewData()}>Create new data</button>
               </td>
             </tr>
           </thead>
-          <tbody>
-           { myData }
-          </tbody>
+          <tbody>{myData}</tbody>
         </table>
       </div>
     );
--- src/TableItem/TableItem.js
+++ src/TableItem/TableItem.js
@@ -6,9 +6,9 @@ const tableItem = props => {
     <tr>
       <td>
         <p>{props.title}</p>
-        <input type="text" id="changeTitle" placeholder="Change Title" />
-        <input type="button" id="changeConfirm" value="Change Confirm" />
-        <input type="button" id="delete" value="Delete"  onClick={props.delete} />
+        <input type="text" className="changeTitle" placeholder="Change Title" />
+        <input type="button" id="changeConfirm" value="Change Confirm" onClick={props.changeTitle} />
+        <input type="button" id="delete" value="Delete" onClick={props.delete} />
       </td>
     </tr>
   );
